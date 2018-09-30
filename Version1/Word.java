6a046648b4b56598511fb24c0fc8735ff9800d0b
duchuan28 <41351925+duchuan28@users.noreply.github.com>
2018-09-30 19:57:59 +0700

    Create Word.java

---
+++ Version1/Word.java
@@ -0,0 +1,23 @@
+package version1;
+
+public class Word{ 
+    protected String word_target;
+    protected String word_explain;
+    
+// Phương thức get-set
+    public String getWord_target() {
+        return word_target;
+    }
+
+    public void setWord_target(String word_target) {
+        this.word_target = word_target;
+    }
+
+    public String getWord_explain() {
+        return word_explain;
+    }
+
+    public void setWord_explain(String word_explain) {
+        this.word_explain = word_explain;
+    }
+}
