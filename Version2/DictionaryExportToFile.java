bda07c8c87e83b9cf17974430fedb5b96cc129ac
duchuan28 <41351925+duchuan28@users.noreply.github.com>
2018-10-03 13:58:21 +0700

    Create DictionaryExportToFile.java

---
+++ Version2/DictionaryExportToFile.java
@@ -0,0 +1,37 @@
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DictionaryExportToFile {
+// Hàm ghiFile(list) ghi lại từ vừa thêm vào file
+    public void ghiFile(ArrayList <Word> list){
+        File file= new File("dictionaries.txt");
+        try (PrintWriter print = new PrintWriter(file)){
+            for(Word i: list){
+                print.println(i.getWord_target());
+                print.println(i.getWord_explain());
+            }
+            print.close();
+        } catch (Exception e) {
+            System.out.println("File trống!");
+        }
+    }
+// Hàm docFile() đọc dữ liệu từ file
+    public ArrayList docFile(){
+        File file= new File("dictionaries.txt");
+        ArrayList<Word> list = new ArrayList<>();
+        try(Scanner scan = new Scanner(file)) {
+            while(scan.hasNext()){
+                Word w = new Word();
+                w.setWord_target(scan.nextLine());
+                w.setWord_explain(scan.nextLine());
+                list.add(w);
+            }
+        } catch (Exception e) {
+            System.out.println("File trống!");
+        }
+        return list;
+    }
+}
