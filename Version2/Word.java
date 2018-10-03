05c4950454f40a07add2eef4888cc4a0551557b4
duchuan28 <41351925+duchuan28@users.noreply.github.com>
2018-10-03 14:00:06 +0700

    Update Word.java

--- Version2/Word.java
+++ Version2/Word.java
@@ -6,13 +6,6 @@ public class Word implements Serializable{
 // Serializable chuyển đổi các đối tượng thành stream.
     protected String word_target;
     protected String word_explain;
-// Khởi tạo không đối tượng
-    public Word(){}
-// Khởi tạo đầy đủ
-    public Word(String word_target, String word_explain) {
-        this.word_target = word_target;
-        this.word_explain = word_explain;
-    }
 // Phương thức get-set
     public String getWord_target() {
         return word_target;
