193ec5e3d734298d026d72e8e9b704cd3926703c
duchuan28 <41351925+duchuan28@users.noreply.github.com>
2018-10-03 12:23:14 +0700

    Update DocGhiDuLieu.java

--- Version2/DocGhiDuLieu.java
+++ Version2/DocGhiDuLieu.java
@@ -1,30 +1,32 @@
 package version2;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DocGhiDuLieu {
-    public void ghiFile(ArrayList list, String filename ){
-        try(FileOutputStream fos = new FileOutputStream(new File(filename))){
-            ObjectOutputStream oos = new ObjectOutputStream(fos);
-            oos.writeObject(list);
-        }catch(Exception e){
+    public void ghiFile(ArrayList <Word> list){
+        try (PrintWriter print = new PrintWriter(new File("dictionaries.txt"))){
+            for(Word i: list){
+                print.println(i.getWord_target());
+                print.println(i.getWord_explain());
+            }
+            print.close();
+        } catch (Exception e) {
             System.out.println("File trống!");
         }
     }
-    
-    public ArrayList docFile(String filename){
-        ArrayList list = new ArrayList();
-        try(FileInputStream fis = new FileInputStream(new File(filename))){
-            ObjectInputStream ois = new ObjectInputStream(fis);
-            list = (ArrayList)ois.readObject();
-        }catch(Exception e){
+    public ArrayList docFile(){
+        ArrayList<Word> list = new ArrayList<>();
+        try(Scanner scan = new Scanner(new File("dictionaries.txt"))) {
+            while(scan.hasNext()){
+                Word w = new Word();
+                w.setWord_target(scan.nextLine());
+                w.setWord_explain(scan.nextLine());
+                list.add(w);
+            }
+        } catch (Exception e) {
             System.out.println("File trống!");
         }
         return list;
